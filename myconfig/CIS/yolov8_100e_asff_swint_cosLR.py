c9079dbbaecc5d6b6011334b84516bb4ad321da0
gaobiaoli <1258497127@qq.com>
2024-01-30 20:51:38 +0800

    1-30-2

---
+++ myconfig/CIS/yolov8_100e_asff_swint_cosLR.py
@@ -0,0 +1,186 @@
+_base_ = "../../configs/yolov8/yolov8_s_syncbn_fast_8xb16-500e_coco.py"
+
+metainfo = dict(
+    classes=(
+        "PC",
+        "PC-truck",
+        "dozer",
+        "dump-truck",
+        "excavator",
+        "mixer",
+        "people-helmet",
+        "people-no-helmet",
+        "roller",
+        "wheel-loader",
+    )
+)
+data_root = "/CV/gaobiaoli/dataset/CIS-Dataset"
+resume = False
+work_dir = "work_dirs"
+channels = [192, 384, 768]
+checkpoint_file = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
+widen_factor=1
+model = dict(
+    backbone=dict(
+        _delete_=True, # 将 _base_ 中关于 backbone 的字段删除
+        type='mmdet.SwinTransformer', # 使用 mmdet 中的 SwinTransformer
+        embed_dims=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file)),
+    bbox_head=dict(
+        head_module=dict(
+            in_channels=channels,
+            num_classes=10,
+            type='YOLOv8HeadModule',
+            widen_factor=widen_factor)
+        ),
+    neck=[
+        dict(
+            deepen_factor=_base_.deepen_factor,
+            in_channels=channels,
+            out_channels=channels,
+            type='YOLOv8PAFPN',
+            widen_factor=widen_factor),
+        dict(type="ASFFNeck", widen_factor=widen_factor*0.75, use_att="ASFF_sim"),
+    ],
+    train_cfg=dict(assigner=dict(num_classes=10)),
+)
+
+train_dataloader = dict(
+    batch_size=16,
+    dataset=dict(
+        ann_file="annotations/train.json",
+        backend_args=None,
+        data_prefix=dict(img="train"),
+        data_root=data_root,
+        metainfo=metainfo,
+    ),
+    num_workers=16,
+)
+
+# auto_scale_lr = dict(enable=True, base_batch_size=8 * 16)
+default_hooks = dict(
+    logger=dict(interval=200, type='LoggerHook'),
+    param_scheduler=dict(lr_factor=0.01, max_epochs=100, scheduler_type="mix")
+)
+train_cfg = dict(
+    dynamic_intervals=[
+        (
+            90,
+            1,
+        ),
+    ],
+    max_epochs=100,
+    type="EpochBasedTrainLoop",
+    val_interval=10,
+)
+custom_hooks = [
+    dict(
+        switch_epoch=90,
+        switch_pipeline=[
+            dict(backend_args=None, type="LoadImageFromFile"),
+            dict(type="LoadAnnotations", with_bbox=True),
+            dict(
+                scale=(
+                    640,
+                    640,
+                ),
+                type="YOLOv5KeepRatioResize",
+            ),
+            dict(
+                allow_scale_up=True,
+                pad_val=dict(img=114.0),
+                scale=(
+                    640,
+                    640,
+                ),
+                type="LetterResize",
+            ),
+            dict(
+                border_val=(
+                    114,
+                    114,
+                    114,
+                ),
+                max_aspect_ratio=100,
+                max_rotate_degree=0.0,
+                max_shear_degree=0.0,
+                scaling_ratio_range=(
+                    0.5,
+                    1.5,
+                ),
+                type="YOLOv5RandomAffine",
+            ),
+            dict(
+                bbox_params=dict(
+                    format="pascal_voc",
+                    label_fields=[
+                        "gt_bboxes_labels",
+                        "gt_ignore_flags",
+                    ],
+                    type="BboxParams",
+                ),
+                keymap=dict(gt_bboxes="bboxes", img="image"),
+                transforms=[
+                    dict(p=0.01, type="Blur"),
+                    dict(p=0.01, type="MedianBlur"),
+                    dict(p=0.01, type="ToGray"),
+                    dict(p=0.01, type="CLAHE"),
+                ],
+                type="mmdet.Albu",
+            ),
+            dict(type="YOLOv5HSVRandomAug"),
+            dict(prob=0.5, type="mmdet.RandomFlip"),
+            dict(
+                meta_keys=(
+                    "img_id",
+                    "img_path",
+                    "ori_shape",
+                    "img_shape",
+                    "flip",
+                    "flip_direction",
+                ),
+                type="mmdet.PackDetInputs",
+            ),
+        ],
+        type="mmdet.PipelineSwitchHook",
+    ),
+]
+
+test_dataloader = dict(
+    batch_size=2,
+    dataset=dict(
+        ann_file="annotations/test.json",
+        backend_args=None,
+        data_prefix=dict(img="test"),
+        data_root=data_root,
+        metainfo=metainfo,
+    ),
+)
+test_evaluator = dict(ann_file=data_root + "/annotations/test.json")
+
+val_dataloader = dict(
+    batch_size=2,
+    dataset=dict(
+        ann_file="annotations/val.json",
+        backend_args=None,
+        data_prefix=dict(img="val"),
+        data_root=data_root,
+        metainfo=metainfo,
+    ),
+)
+val_evaluator = dict(
+    ann_file=data_root + "/annotations/val.json",
+)
